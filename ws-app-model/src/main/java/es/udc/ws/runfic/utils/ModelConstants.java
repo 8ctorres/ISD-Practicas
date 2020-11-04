a639a5bf691a614ef04df856a6df297bef41bc8d
8ctorres <carlos.torres@udc.es>
2020-11-04 09:11:47 +0100

    ModelConstants

---
+++ ws-app-model/src/main/java/es/udc/ws/runfic/utils/ModelConstants.java
@@ -0,0 +1,8 @@
+package es.udc.ws.runfic.utils;
+
+public final class ModelConstants {
+    private ModelConstants(){};
+
+    public static final int MAX_PRICE = 999;
+    public static final int MAX_PARTICIPANTS = 99999;
+}
