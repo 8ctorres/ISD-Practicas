a9739c185dc880bc6d8452d65b9479812e0eab55
Brais <brais.vazquez.villa@udc.es>
2020-11-18 09:29:56 +0100

    createRace Test

--- ws-app-model/src/test/java/es/udc/ws/runfic/test/model/runservice/RunServiceTest.java
+++ ws-app-model/src/test/java/es/udc/ws/runfic/test/model/runservice/RunServiceTest.java
@@ -1,5 +1,6 @@
 package es.udc.ws.runfic.test.model.runservice;
 
+import es.udc.ws.runfic.model.race.Race;
 import es.udc.ws.runfic.model.runservice.RunService;
 import es.udc.ws.runfic.model.runservice.RunServiceImpl;
 import es.udc.ws.runfic.model.runservice.RunServiceFactory;
@@ -7,12 +8,15 @@ import es.udc.ws.runfic.model.race.SqlRaceDao;
 import es.udc.ws.runfic.model.race.SqlRaceDaoFactory;
 import static es.udc.ws.runfic.utils.ModelConstants.RACE_DATA_SOURCE;
 
+import es.udc.ws.util.exceptions.InputValidationException;
 import org.junit.jupiter.api.BeforeAll;
 
 import es.udc.ws.util.sql.DataSourceLocator;
 import es.udc.ws.util.sql.SimpleDataSource;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 public class RunServiceTest {
 
@@ -34,6 +38,26 @@ public class RunServiceTest {
         runService = RunServiceFactory.getService();
 
         raceDao = SqlRaceDaoFactory.getDao();
+
+    }
+
+    //Brais
+    private Race createRace(Race race) {
+
+        String city = race.getCity();
+        String description = race.getDescription();
+        int maxParticipants = race.getMaxParticipants();
+        BigDecimal price = race.getPrice();
+        LocalDateTime startDateTime = race.getStartDateTime();
+
+        Race addedRace = null;
+
+        try {
+            addedRace = runService.addRace(city, description, startDateTime, price, maxParticipants);
+        } catch (InputValidationException e) {
+            throw new RuntimeException(e);
+        }
+        return addedRace;
 
     }
 
