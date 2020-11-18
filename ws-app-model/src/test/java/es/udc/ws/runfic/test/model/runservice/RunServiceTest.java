a486e0990b2fd5a7a68c3ca864257ca8bc2c365c
Brais <brais.vazquez.villa@udc.es>
2020-11-18 09:17:49 +0100

    Creado archivo test

---
+++ ws-app-model/src/test/java/es/udc/ws/runfic/test/model/runservice/RunServiceTest.java
@@ -0,0 +1,40 @@
+package es.udc.ws.runfic.test.model.runservice;
+
+import es.udc.ws.runfic.model.runservice.RunService;
+import es.udc.ws.runfic.model.runservice.RunServiceImpl;
+import es.udc.ws.runfic.model.runservice.RunServiceFactory;
+import es.udc.ws.runfic.model.race.SqlRaceDao;
+import es.udc.ws.runfic.model.race.SqlRaceDaoFactory;
+import static es.udc.ws.runfic.utils.ModelConstants.RACE_DATA_SOURCE;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import es.udc.ws.util.sql.DataSourceLocator;
+import es.udc.ws.util.sql.SimpleDataSource;
+
+import javax.sql.DataSource;
+
+public class RunServiceTest {
+
+    private static RunService runService = null;
+    private static SqlRaceDao raceDao = null;
+
+    @BeforeAll
+    public static void init() {
+
+        /*
+         * Create a simple data source and add it to "DataSourceLocator" (this
+         * is needed to test "es.udc.ws.movies.model.movieservice.MovieService"
+         */
+        DataSource dataSource = new SimpleDataSource();
+
+        /* Add "dataSource" to "DataSourceLocator". */
+        DataSourceLocator.addDataSource(RACE_DATA_SOURCE, dataSource);
+
+        runService = RunServiceFactory.getService();
+
+        raceDao = SqlRaceDaoFactory.getDao();
+
+    }
+
+}
