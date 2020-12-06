96dcb7405f444bf0e497a21a6f29320d11dc7074
IsmaVerde <ismael.verdec@udc.es>
2020-12-06 13:31:11 +0100

    [COMÚN] añadidas dos excepciones a JSON

--- ws-app-service/src/main/java/es/udc/ws/runfic/rest/json/ExceptionToJsonConverter.java
+++ ws-app-service/src/main/java/es/udc/ws/runfic/rest/json/ExceptionToJsonConverter.java
@@ -1,6 +1,7 @@
 package es.udc.ws.runfic.rest.json;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import es.udc.ws.runfic.model.inscription.Inscription;
 import es.udc.ws.runfic.model.runservice.exceptions.*;
 import es.udc.ws.util.exceptions.InputValidationException;
@@ -10,11 +11,26 @@ public class ExceptionToJsonConverter {
     private ExceptionToJsonConverter(){}
 
     public static ObjectNode from(InputValidationException ex){
-        throw new UnsupportedOperationException();
+
+        ObjectNode exceptionObject = JsonNodeFactory.instance.objectNode();
+
+        exceptionObject.put("errorType", "InputValidation");
+        exceptionObject.put("message", ex.getMessage());
+
+        return exceptionObject;
     }
 
     public static ObjectNode from(InstanceNotFoundException ex){
-        throw new UnsupportedOperationException();
+
+        ObjectNode exceptionObject = JsonNodeFactory.instance.objectNode();
+
+        exceptionObject.put("errorType", "InstanceNotFound");
+        exceptionObject.put("instanceId", (ex.getInstanceId() != null) ?
+                ex.getInstanceId().toString() : null);
+        exceptionObject.put("instanceType",
+                ex.getInstanceType().substring(ex.getInstanceType().lastIndexOf('.') + 1));
+
+        return exceptionObject;
     }
 
     public static ObjectNode from(AlreadyInscribedException ex){
