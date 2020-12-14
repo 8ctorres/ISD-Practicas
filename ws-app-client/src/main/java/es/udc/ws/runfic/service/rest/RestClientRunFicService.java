6738ff0e612ebcc600bd52362a22dc62aa4c5a86
8ctorres <carlos.torres@udc.es>
2020-12-14 17:28:41 +0100

    [Comun] Pequeños comentarios en el cliente

--- ws-app-client/src/main/java/es/udc/ws/runfic/service/rest/RestClientRunFicService.java
+++ ws-app-client/src/main/java/es/udc/ws/runfic/service/rest/RestClientRunFicService.java
@@ -12,31 +12,37 @@ import java.util.List;
 public class RestClientRunFicService implements ClientRunFicService {
 
     @Override
+    //Caso de Uso 1 - Brais
     public Long addRace(ClientRaceDto race) throws InputValidationException {
         throw new UnsupportedOperationException();
     }
 
     @Override
+    //Caso de Uso 2 - Isma
     public ClientRaceDto findRace(Long raceID) throws InputValidationException, InstanceNotFoundException {
         throw new UnsupportedOperationException();
     }
 
     @Override
+    //Caso de Uso 3 - Brais
     public List<ClientRaceDto> findByDate(LocalDateTime date, String city) {
         throw new UnsupportedOperationException();
     }
 
     @Override
+    //Caso de Uso 4 - Carlos
     public ClientInscriptionDto inscribe(Long raceID, String email, String creditCardNumber) throws InputValidationException, InstanceNotFoundException {
         throw new UnsupportedOperationException();
     }
 
     @Override
+    //Caso de Uso 5 - Carlos
     public List<ClientInscriptionDto> findAllFromUser(String email) throws InputValidationException {
         throw new UnsupportedOperationException();
     }
 
     @Override
+    //Caso de Uso 6 - Isma
     public int getRunnerNumber(String email, Long inscriptionID, String creditCardNumber) throws InputValidationException, InstanceNotFoundException {
         throw new UnsupportedOperationException();
     }
