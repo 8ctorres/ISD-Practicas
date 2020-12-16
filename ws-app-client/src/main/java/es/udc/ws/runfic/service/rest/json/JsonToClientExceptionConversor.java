ddaa4a3ff908e22de57f5617617f9ccb57583119
IsmaVerde <ismael.verdec@udc.es>
2020-12-16 12:45:53 +0100

    [COMÚN] implementadas dos excepciones a JsonToClientExceptionConversor

--- ws-app-client/src/main/java/es/udc/ws/runfic/service/rest/json/JsonToClientExceptionConversor.java
+++ ws-app-client/src/main/java/es/udc/ws/runfic/service/rest/json/JsonToClientExceptionConversor.java
@@ -1,14 +1,68 @@
 package es.udc.ws.runfic.service.rest.json;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import es.udc.ws.util.exceptions.InputValidationException;
+import es.udc.ws.util.exceptions.InstanceNotFoundException;
+import es.udc.ws.util.json.ObjectMapperFactory;
+import es.udc.ws.util.json.exceptions.ParsingException;
+
 import java.io.InputStream;
 
 public class JsonToClientExceptionConversor {
+    //Isma
     public static Exception fromNotFoundErrorCode(InputStream content) {
-        throw new UnsupportedOperationException();
+        try {
+            ObjectMapper objectMapper = ObjectMapperFactory.instance();
+            JsonNode rootNode = objectMapper.readTree(content);
+            if (rootNode.getNodeType() != JsonNodeType.OBJECT) {
+                throw new ParsingException("Unrecognized JSON (object expected)");
+            } else {
+                String errorType = rootNode.get("errorType").textValue();
+                if (errorType.equals("InstanceNotFound")) {
+                    return toInstanceNotFoundException(rootNode);
+                } else {
+                    throw new ParsingException("Unrecognized error type: " + errorType);
+                }
+            }
+        } catch (ParsingException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ParsingException(e);
+        }
     }
 
+    private static InstanceNotFoundException toInstanceNotFoundException(JsonNode rootNode) {
+        String instanceId = rootNode.get("instanceId").textValue();
+        String instanceType = rootNode.get("instanceType").textValue();
+        return new InstanceNotFoundException(instanceId, instanceType);
+    }
+
+    //Isma
     public static Exception fromBadRequestErrorCode(InputStream content) {
-        throw new UnsupportedOperationException();
+        try {
+            ObjectMapper objectMapper = ObjectMapperFactory.instance();
+            JsonNode rootNode = objectMapper.readTree(content);
+            if (rootNode.getNodeType() != JsonNodeType.OBJECT) {
+                throw new ParsingException("Unrecognized JSON (object expected)");
+            } else {
+                String errorType = rootNode.get("errorType").textValue();
+                if (errorType.equals("InputValidation")) {
+                    return toInputValidationException(rootNode);
+                } else {
+                    throw new ParsingException("Unrecognized error type: " + errorType);
+                }
+            }
+        } catch (ParsingException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new ParsingException(e);
+        }
+    }
+    private static InputValidationException toInputValidationException(JsonNode rootNode) {
+        String message = rootNode.get("message").textValue();
+        return new InputValidationException(message);
     }
 
     public static Exception fromGoneErrorCode(InputStream content) {
